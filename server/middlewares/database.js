2e609f918775d8a7a0b93ff078b1d2e4487df11d
minhh <minhhaivuhd@gmail.com>
2020-03-28 13:47:28 -0400

    connect to mongoDB database locally

---
+++ server/middlewares/database.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const DB_HOST ='localhost';
+const DB_PORT ='27017';
+const DB_NAME ='dev-kanban';
+const DB_USER_NAME='';
+const DB_PASSWORD ='';
+const MONGODB_URI ='mongodb://'+ DB_HOST +':' + DB_PORT +'/' + DB_NAME;
+
+const connectToDatabase = async () => {
+    try {
+        const conn = await mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        console.log(`MongoDB connected : ${conn.connection.host}`);
+
+    } catch (e) { //If failed to connect
+        console.log(`MongoDB failed to connect. ${e.message}`);
+    }
+};
+
+module.exports = connectToDatabase;
