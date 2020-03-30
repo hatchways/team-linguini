9eccea718a949a924d14f63495524615db668af7
minhh <minhhaivuhd@gmail.com>
2020-03-30 18:02:22 -0400

    validate forms on login/signup page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,10 +3,18 @@ import React, {Fragment} from "react";
 import {AuthForm, RedirectDiv} from './Signup'
 
 const Login = () => {
+    //Callback for the form submission after validating successfully
+    const onSubmit = () => {
+        //Make request to check whether the email and password are valid
+
+        //Redirect to dashboard
+        window.location.replace('/');
+    }
+
     return (
         <Fragment>
             <div>Welcome to sign up page</div>
-            <AuthForm title="Welcome back" input1="Enter email" input2="Password" submit="Login"/>
+            <AuthForm title="Welcome back" input1="Enter email" input2="Password" submit="Login" onSubmit={onSubmit}/>
             <RedirectDiv title={'Don\'t have an account?'} link={'/signup'} desc={'Create'}/>
         </Fragment>
     )
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,14 +1,41 @@
 import React, {Fragment} from "react";
 import {Link} from "react-router-dom";
+import { useForm } from 'react-hook-form';
 
-const AuthForm = (props) => (
-    <div className={'form-group'}>
-        <div>{props.title}</div>
-        <input className={'form-input'} type="text" placeholder={props.input1}/><br/>
-        <input className={'form-input'} type="text" placeholder={props.input2}/><br/>
-        <input className={'form-submit'} type={'submit'} value={props.submit}/>
-    </div>
-);
+
+const AuthForm = (props) => {
+    const { handleSubmit, register, errors } = useForm();
+
+    const emailValidator = {
+        required: 'Email is required',
+        pattern: {
+            value: /^\S+@\S+$/i,
+            message: "Invalid email address"
+        }
+    }
+
+    const passwordValidator = {
+        required: 'Password is required',
+        minLength: {
+            value: 6,
+            message: "Password needs 6 characters at least"
+        }
+    }
+
+    return (
+        <form className={'form-group'} onSubmit={handleSubmit(props.onSubmit)}>
+            <div>{props.title}</div>
+
+            <input name="email" className={'form-input'} type="text" placeholder={props.input1} ref={register(emailValidator)}/><br/>
+            <label>{errors.email && errors.email.message}</label><br />
+
+            <input name="password" className={'form-input'} type="password" placeholder={props.input2} ref={register(passwordValidator)}/><br/>
+            <label>{errors.password && errors.password.message}</label><br />
+
+            <input className={'form-submit'} type={'submit'} value={props.submit}/>
+        </form>
+    )
+};
 
 const RedirectDiv = (props) => (
     <div>
@@ -18,10 +45,37 @@ const RedirectDiv = (props) => (
 )
 
 const Signup = () => {
+    //Callback for the form submission after validation
+    const onSubmit = values => {
+        const {email, password} = values;
+        console.log(email, password);
+
+        //There is no user api to call at the moment. So I assumed that the user received a successful response
+        //for creating the new user on server.
+        //Therefore, it will redirect to login page
+        // const url = window.location.hostname +'/login';
+        // console.log('url', url);
+        window.location.replace('/login');
+
+
+
+        //Call api
+        // const domain = 'http://localhost/'
+        // const url = domain + '/users/';
+        // const data = {email, password};
+        // axios.post(url, data)
+        //     .then(res => {
+        //         If success to create a new account, redirect to login page
+
+        // If failed to create a new account
+        // })
+        // .catch(errors => console.log(errors.message));
+    };
+
     return (
         <Fragment>
             <div>Welcome to sign up page</div>
-            <AuthForm title="Sign up to Kanban" input1="Enter email" input2="Create Password" submit="Sign up"/>
+            <AuthForm title="Sign up to Kanban" input1="Enter email" input2="Create Password" submit="Sign up" onSubmit={onSubmit}/>
             <RedirectDiv title={'Already have an account?'} link={'/login'} desc={'Login'}/>
         </Fragment>
     )
