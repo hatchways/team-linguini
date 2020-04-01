1621f2c33b3c543844ef363361fa16056cdd3b09
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-04-01 08:30:30 -0500

    Added more dependencies

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const { hash, compare } = require('bcrypt');
 
+router.use(express.json());
+router.use(express.urlencoded({ extended: true}));
 router.get("/welcome", function(req, res, next) {
   res.status(200).send({ welcomeMessage: "Step 1 (completed)" });
 });
 
+router.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const hashedPassowrd = await hash(password, 10);
+    console.log(hashedPassowrd)
+  } catch (err) {
+    console.log(err)
+  }
+})
 module.exports = router;
