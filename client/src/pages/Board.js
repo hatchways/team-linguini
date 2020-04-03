fb1062d2217dca464c4878893ed44a5328d455a3
aslamm <aslamm.anas29@gmail.com>
2020-04-02 23:21:39 -0400

    Added the drag and drop feature of the cards

--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
@@ -33,23 +33,23 @@ const useStyles = makeStyles(theme => ({
     background: "#F4F6FF",
     boxShadow: "none",
     position: "relative",
-    marginRight: "20px",
+    marginRight: "20px"
   },
   grid: {
     marginTop: "40px",
     marginLeft: "20px",
-    display: 'flex',
+    display: "flex"
   },
   container: {
-    width: '100%',
-    maxWidth: '1370px',
-    overflowX: 'scroll',
-    overflow: 'hidden',
-    margin: '0 auto',
-    paddingBottom: '17px',
+    width: "100%",
+    maxWidth: "1370px",
+    overflowX: "scroll",
+    overflow: "hidden",
+    margin: "0 auto",
+    paddingBottom: "17px",
     "&::-webkit-scrollbar": {
-        display: 'none'
-      },
+      display: "none"
+    }
   },
   column: {
     marginLeft: "25px",
@@ -87,11 +87,11 @@ const useStyles = makeStyles(theme => ({
   },
   cardSection: {
     height: 410,
-    overflow: 'hidden',
-    overflowY: 'scroll',
+    overflow: "hidden",
+    overflowY: "scroll",
     "&::-webkit-scrollbar": {
-        display: 'none'
-      },
+      display: "none"
+    }
   },
   addCard: {
     background: "#759CFC",
@@ -108,39 +108,99 @@ const useStyles = makeStyles(theme => ({
 
 //Cards Data Structure
 const cardsData = [
-    {id: '1', title: 'sample Title', deadline: '07/04/2020', tag: 'green', description: 'lorem ipsum'},
-    {id: '2', title: 'sample Title', deadline: '07/04/2020', tag: 'green', description: 'lorem ipsum'},
-    {id: '3', title: 'sample Title', deadline: '07/04/2020', tag: 'green', description: 'lorem ipsum'},
-    {id: '4', title: 'sample Title', deadline: '07/04/2020', tag: 'green', description: 'lorem ipsum'}
-]
+  {
+    id: "1",
+    title: "Study Math",
+    deadline: "07/04/2020",
+    tag: "green",
+    description: "lorem ipsum"
+  },
+  {
+    id: "2",
+    title: "Go to School",
+    deadline: "07/04/2020",
+    tag: "green",
+    description: "lorem ipsum"
+  },
+  {
+    id: "3",
+    title: "Do your Homework",
+    deadline: "07/04/2020",
+    tag: "green",
+    description: "lorem ipsum"
+  },
+  {
+    id: "4",
+    title: "Eat your veggies",
+    deadline: "07/04/2020",
+    tag: "green",
+    description: "lorem ipsum"
+  }
+];
 
 //Column Data Structure
 const columnsData = {
-    1: {
-        title: 'Backlog',
-        cards: cardsData,
-        columnOrder: 1
-    },
-    2: {
-        title: 'In Progress',
-        cards: cardsData,
-        columnOrder: 1
-    },
-    3: {
-        title: 'Review',
-        cards: cardsData,
-        columnOrder: 1
-    },
-    4: {
-        title: 'Completed',
-        cards: cardsData,
-        columnOrder: 1
-    },
-}
+  1: {
+    title: "Backlog",
+    cards: cardsData,
+    columnOrder: 1
+  },
+  2: {
+    title: "In Progress",
+    cards: [],
+    columnOrder: 1
+  },
+  3: {
+    title: "Review",
+    cards: [],
+    columnOrder: 1
+  },
+  4: {
+    title: "Completed",
+    cards: [],
+    columnOrder: 1
+  }
+};
 
 const Board = () => {
   const classes = useStyles();
-  const [columns, setColumns] = useState(columnsData)
+  const [columns, setColumns] = useState(columnsData);
+
+  const onDragEnd = ((result, columns, setColumns) => {
+      if(!result.destination) return;
+      const { source, destination } = result
+      if(source.droppableId !== destination.droppableId){
+        const sourceColumn = columns[source.droppableId]
+        const destinationColumn = columns[destination.droppableId]
+        const sourceCards = [...sourceColumn.cards]
+        const destinationCards = [...destinationColumn.cards]
+        const [removed] = sourceCards.splice(source.index, 1)
+        destinationCards.splice(destination.index, 0, removed)
+        setColumns({
+            ...columns,
+            [source.droppableId]: {
+                ...sourceColumn,
+                cards: sourceCards
+            },
+            [destination.droppableId]: {
+                ...destinationColumn,
+                cards: destinationCards
+            }
+        })
+      }else{
+      const column = columns[source.droppableId]
+      const copiedCards = [...column.cards]
+      const [removed] = copiedCards.splice(source.index, 1)
+      copiedCards.splice(destination.index, 0, removed)
+      setColumns({
+          ...columns,
+          [source.droppableId]: {
+              ...column,
+              cards: copiedCards
+          }
+      })
+    }
+  })
 
   return (
     <div>
@@ -161,79 +221,82 @@ const Board = () => {
       </AppBar>
 
       <div className={classes.container}>
-      <div
-        className={classes.grid}
-      >
-        {[0, 1, 2, 3, 4, 5].map(value => (
-        <DragDropContext>
-          <Grid key={value} item>
-            <Paper className={classes.paper}>
-              <div className={classes.columnTitle}>
-                <Typography variant="h6" className={classes.column}>
-                  In Progress
-                </Typography>
-                <div className={classes.icon}>
-                  <i
-                    className="fas fa-ellipsis-h"
-                    style={{ color: "#D7DDF8" }}
-                  ></i>
-                </div>
-              </div>
+        <div className={classes.grid}>
+          <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
+            {Object.entries(columns).map(([id, column]) => (
+              <Droppable droppableId={id} key={id}>
+                {(provided, snapshot) => {
+                  return (
+                    <div {...provided.droppableProps} ref={provided.innerRef}>
+                      <Grid key={id} item>
+                        <Paper className={classes.paper}>
+                          <div className={classes.columnTitle}>
+                            <Typography variant="h6" className={classes.column}>
+                              {column.title}
+                            </Typography>
+                            <div className={classes.icon}>
+                              <i
+                                className="fas fa-ellipsis-h"
+                                style={{ color: "#D7DDF8" }}
+                              ></i>
+                            </div>
+                          </div>
 
-            <div className={classes.cardSection}>
-              <Card className={classes.card}>
-                <CardContent>
-                  <div className={classes.tag} />
-                  <Typography className={classes.cardTitle} gutterBottom>
-                    Word of the Day
-                  </Typography>
-                  <Typography color="textSecondary">April 10</Typography>
-                </CardContent>
-              </Card>
-              <Card className={classes.card}>
-                <CardContent>
-                  <div className={classes.tag} />
-                  <Typography className={classes.cardTitle} gutterBottom>
-                    Word of the Day
-                  </Typography>
-                  <Typography color="textSecondary">April 10</Typography>
-                </CardContent>
-              </Card>
-              <Card className={classes.card}>
-                <CardContent>
-                  <div className={classes.tag} />
-                  <Typography className={classes.cardTitle} gutterBottom>
-                    Word of the Day
-                  </Typography>
-                  <Typography color="textSecondary">April 10</Typography>
-                </CardContent>
-              </Card>
-              <Card className={classes.card}>
-                <CardContent>
-                  <div className={classes.tag} />
-                  <Typography className={classes.cardTitle} gutterBottom>
-                    Word of the Day
-                  </Typography>
-                  <Typography color="textSecondary">April 10</Typography>
-                </CardContent>
-              </Card>
-              <Card className={classes.card}>
-                <CardContent>
-                  <div className={classes.tag} />
-                  <Typography className={classes.cardTitle} gutterBottom>
-                    Word of the Day
-                  </Typography>
-                  <Typography color="textSecondary">April 10</Typography>
-                </CardContent>
-              </Card>
-              </div>
-              <Button variant="contained" className={classes.addCard}>
-                Add Card
-              </Button>
-            </Paper>
-          </Grid>
+                          <div className={classes.cardSection}>
+                            {column.cards.map((card, index) => {
+                              return (
+                                <Draggable
+                                  key={card.id}
+                                  draggableId={card.id}
+                                  index={index}
+                                >
+                                  {(provided, snapshot) => {
+                                    return (
+                                      <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps} 
+                                        style={{
+                                          userSelect: "none",
+                                          ...provided.draggableProps.style
+                                        }}
+                                      >
+                                        <Card className={classes.card}>
+                                          <CardContent>
+                                            <div className={classes.tag} />
+                                            <Typography
+                                              className={classes.cardTitle}
+                                              gutterBottom
+                                            >
+                                              {card.title}
+                                            </Typography>
+                                            <Typography color="textSecondary">
+                                              {card.deadline}
+                                            </Typography>
+                                          </CardContent>
+                                        </Card>
+                                      </div>
+                                    );
+                                  }}
+                                </Draggable>
+                              );
+                            })}
+                          </div>
+                          <Button
+                            variant="contained"
+                            className={classes.addCard}
+                          >
+                            Add Card
+                          </Button>
+                        </Paper>
+                      </Grid>
+                      {provided.placeholder}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            ))}
           </DragDropContext>
-        ))}
         </div>
       </div>
     </div>
