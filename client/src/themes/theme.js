2413c82e855171c9696b5815fe0bb06138647303
minhh <minhhaivuhd@gmail.com>
2020-04-05 14:36:52 -0400

    fix font family

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -2,7 +2,7 @@ import { createMuiTheme } from "@material-ui/core";
 
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: ["Montserrat", "Roboto"].join(','),
+    fontFamily: ['"Montserrat"'].join(', '),
     fontSize: 12,
     h1: {
       // could customize the h1 variant as well
