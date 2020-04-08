b7185be5f6b1007c7b8666fc486d5257dcce8508
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-04-07 22:48:54 -0500

    Started to finally get buttons to align correctly

--- client/src/Components/NavigationBar.js
+++ client/src/Components/NavigationBar.js
@@ -1,23 +1,41 @@
-import React from './node_modules/react'
+import React from 'react'
 
-import { makeStyles } from "./node_modules/@material-ui/core/styles";
-import AppBar from "./node_modules/@material-ui/core/AppBar";
-import Toolbar from "./node_modules/@material-ui/core/Toolbar";
-import Typography from "./node_modules/@material-ui/core/Typography";
-import IconButton from "./node_modules/@material-ui/core/IconButton";
-import DashboardRoundedIcon from './node_modules/@material-ui/icons/DashboardRounded';
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+import Button from '@material-ui/core/Button';
+import Typography from "@material-ui/core/Typography";
+import DashboardRoundedIcon from '@material-ui/icons/DashboardRounded';
+import CalendarTodayOutlinedIcon from '@material-ui/icons/CalendarTodayOutlined';
+import Icon from '@material-ui/core/Icon';
 
 const useStyles = makeStyles(theme => ({
+  iconButtonLabel: {
+    display: 'flex',
+  },
     appBar: {
-      background: '#5D5D5D',//"#FFFFFF",
-      boxShadow: "none"
+      background: "#FFFFFF",
+      boxShadow: "none",
+      display: 'flex',
+      flexDirection: "row",
+    },
+    toolBar: {
+      //display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    centerDiv: {
+      alignItems: 'center',
     },
     menuButton: {
-        color: '#6D6D6D',
-        marginRight: theme.spacing(2)
+        color: 'black',
+    },
+    button: {
+      margin: theme.spacing(1),
     },
     title: {
-      flexGrow: 1
+      color: 'black',
     }
   }));
 
@@ -25,18 +43,17 @@ const NavigationBar = () => {
     const classes = useStyles();
     return(
         <AppBar position="static" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            My School Board
+          <Typography variant="title">
+            <img src="/images/logo.png" alt="bug"  />
           </Typography>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <DashboardRoundedIcon />
-          </IconButton>
+          <Toolbar className = {classes.toolBar}>
+            <Button>
+              <DashboardRoundedIcon/> Dashboard 
+            </Button>
+            <Button>
+              <CalendarTodayOutlinedIcon/> Calendar
+            </Button>
+
         </Toolbar>
       </AppBar>
     )
