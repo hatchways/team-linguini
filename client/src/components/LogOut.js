523c24ab5cc2fc52f8dbb3b31aa7da8e87d0aac7
aslamm <aslamm.anas29@gmail.com>
2020-04-09 18:32:42 -0400

    aligned the code

--- client/src/components/LogOut.js
+++ client/src/components/LogOut.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
 const LogOut = () => {
-return(
-    <Route render={props => (<Redirect to={{ pathname: '/login', state: { from: props.location } }} />)} />
-)
-}
+  return (
+    <Route
+      render={(props) => (
+        <Redirect
+          to={{ pathname: "/login", state: { from: props.location } }}
+        />
+      )}
+    />
+  );
+};
 
-export default LogOut
+export default LogOut;
