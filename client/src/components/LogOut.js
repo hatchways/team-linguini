5a4c6a58d12a00817b4a460865b50996f12bde18
aslamm <aslamm.anas29@gmail.com>
2020-04-09 18:35:19 -0400

    removing authentication from the local storage

--- client/src/components/LogOut.js
+++ client/src/components/LogOut.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
 const LogOut = () => {
+    localStorage.removeItem('isAuthenticated')
+    localStorage.removeItem('access_token')
+    localStorage.removeItem('user')
   return (
     <Route
       render={(props) => (
