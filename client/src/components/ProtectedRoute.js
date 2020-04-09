20bbf9e7ceaa401b8d314588ab2b49e4a50425c2
aslamm <aslamm.anas29@gmail.com>
2020-04-09 01:24:36 -0400

    Configured Protected route

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,17 +1,28 @@
-import React, { useContext } from 'react'
-import {} from 'react-router-dom'
-import { AuthContext } from "../providers/auth/auth.provider"
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { AuthContext } from "../providers/auth/auth.provider";
 
-const ProtectedRoute = () => {
-   // const { isAuthenticated, user } = useContext(AuthContext)
-   // console.log(isAuthenticated)
-   // console.log(user)
-    
-    const checkLocalStorage = localStorage.getItem("isAuthenticated")
-    console.log(checkLocalStorage)
-return(
-    <div>ProtectedRoute</div>
-)
-}
+const ProtectedRoute = ({ children, ...rest }) => {
+  // const { isAuthenticated, user } = useContext(AuthContext)
+  // console.log(isAuthenticated)
+  // console.log(user)
 
-export default ProtectedRoute
+  const checkLocalStorage = localStorage.getItem("isAuthenticated");
+  console.log(checkLocalStorage);
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        checkLocalStorage ? (
+          children
+        ) : (
+          <Redirect
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
