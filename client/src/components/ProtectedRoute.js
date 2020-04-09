0aab260da8b83ab3aa256deeb4878dcd62bff52c
aslamm <aslamm.anas29@gmail.com>
2020-04-09 01:13:37 -0400

    checking the local storage for authentication

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {} from 'react-router-dom'
+import { AuthContext } from "../providers/auth/auth.provider"
 
 const ProtectedRoute = () => {
+   // const { isAuthenticated, user } = useContext(AuthContext)
+   // console.log(isAuthenticated)
+   // console.log(user)
+    
+    const checkLocalStorage = localStorage.getItem("isAuthenticated")
+    console.log(checkLocalStorage)
 return(
     <div>ProtectedRoute</div>
 )
