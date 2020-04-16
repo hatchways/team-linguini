c6df6aac25eeffda97f575c48f13d8c9416d6e0f
aslamm <aslamm.anas29@gmail.com>
2020-04-16 00:00:28 -0400

    Added linting

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,57 +1,63 @@
-import React, {useState} from "react";
-import {Grid} from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
-import {useHistory} from "react-router-dom";
-import { AuthForm, RedirectDiv} from "../components/auth"
-import {authStyle} from '../themes/signup.style';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import { AuthForm, RedirectDiv } from "../components/auth";
+import { authStyle } from "../themes/signup.style";
+import axios from "axios";
 
 const Signup = () => {
-    const history = useHistory();
+  const history = useHistory();
 
-    const [serverResponse, setServerResponse] = useState('');
+  const [serverResponse, setServerResponse] = useState("");
 
-    //Classes of CSS style
-    const classes = makeStyles(authStyle)();
+  //Classes of CSS style
+  const classes = makeStyles(authStyle)();
 
-    //Callback for the form submission after validation
-    const onSubmit = values => {
-        const {email, password} = values;
-        console.log(email, password);
+  //Callback for the form submission after validation
+  const onSubmit = (values) => {
+    const { email, password } = values;
+    console.log(email, password);
 
-        //Make a request to backend
-        const url = '/api/v1/auth/register'
-        axios.post(url, {email, password})
-            .then(res => {
+    //Make a request to backend
+    const url = "/api/v1/auth/register";
+    axios
+      .post(url, { email, password })
+      .then((res) => {
+        //If success to create a new account, redirect to login page
+        if (res.status === 200) {
+          history.push("/");
+        } else {
+          setServerResponse(res.data.error);
+        }
+      })
+      .catch((error) => {
+        setServerResponse(error.response.data.error);
+      });
+  };
 
-                //If success to create a new account, redirect to login page
-                if (res.status === 200){
+  return (
+    <Grid container className={classes.vh100}>
+      <Grid item md={6} xs={12} className={classes.img}>
+        {/*<img alt='' src={"/images/image1.png"} className={classes.img}/>*/}
+      </Grid>
+      <Grid item md={6} xs={12}>
+        <AuthForm
+          title="Sign up to Kanban"
+          input1="Enter email"
+          input2="Create Password"
+          submit="Sign up"
+          onSubmit={onSubmit}
+          serverResponse={serverResponse}
+        />
+        <RedirectDiv
+          title={"Already have an account?"}
+          link={"/login"}
+          desc={"Login"}
+        />
+      </Grid>
+    </Grid>
+  );
+};
 
-                    history.push('/')
-                } else {
-                    setServerResponse(res.data.error);
-                }
-
-            })
-            .catch(error => {
-                setServerResponse(error.response.data.error);
-            });
-
-    };
-
-    return (
-        <Grid container className={classes.vh100}>
-            <Grid item md={6} xs={12} className={classes.img}>
-                {/*<img alt='' src={"/images/image1.png"} className={classes.img}/>*/}
-            </Grid>
-            <Grid item md={6} xs={12}>
-                <AuthForm title="Sign up to Kanban" input1="Enter email" input2="Create Password" submit="Sign up"
-                          onSubmit={onSubmit}
-                          serverResponse={serverResponse}/>
-                <RedirectDiv title={'Already have an account?'} link={'/login'} desc={'Login'}/>
-            </Grid>
-        </Grid>
-    )
-}
-
-export {Signup};
+export { Signup };
