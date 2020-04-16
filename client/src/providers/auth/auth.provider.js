edef65e32ad176f9f168a417e807fbea19274bea
aslamm <aslamm.anas29@gmail.com>
2020-04-16 00:02:30 -0400

    Added linting to all client files

--- client/src/providers/auth/auth.provider.js
+++ client/src/providers/auth/auth.provider.js
@@ -1,32 +1,30 @@
-import React, {useReducer} from "react";
-import isAuthenticatedReducer from './isAuthenticated.reducer';
-import userReducer from './user.reducer'
+import React, { useReducer } from "react";
+import isAuthenticatedReducer from "./isAuthenticated.reducer";
+import userReducer from "./user.reducer";
 
 const AuthContext = React.createContext();
 
 const AuthProvider = (props) => {
-    //2 states of AuthProviders to store the state of authentication
-    const preAuthenticated = window.localStorage.getItem('isAuthenticated') || false;
-    const preUserInfo = window.localStorage.getItem('user') || null;
-    const [isAuthenticated, dispatchIsAuthenticated] = useReducer(isAuthenticatedReducer, preAuthenticated);
-    const [user, dispatchUser] = useReducer(userReducer, preUserInfo);
+  //2 states of AuthProviders to store the state of authentication
+  const preAuthenticated =
+    window.localStorage.getItem("isAuthenticated") || false;
+  const preUserInfo = window.localStorage.getItem("user") || null;
+  const [isAuthenticated, dispatchIsAuthenticated] = useReducer(
+    isAuthenticatedReducer,
+    preAuthenticated
+  );
+  const [user, dispatchUser] = useReducer(userReducer, preUserInfo);
 
+  const defaultContext = {
+    isAuthenticated,
+    user,
+    dispatchIsAuthenticated,
+    dispatchUser,
+  };
 
-    const defaultContext = {
-        isAuthenticated,
-        user,
-        dispatchIsAuthenticated,
-        dispatchUser
-    }
-
-    return (
-        <AuthContext.Provider value = {defaultContext} {...props}/>
-    )
-}
+  return <AuthContext.Provider value={defaultContext} {...props} />;
+};
 
 const useAuth = () => React.useContext(AuthContext);
 
-export {
-    useAuth,
-    AuthProvider
-}
+export { useAuth, AuthProvider };
