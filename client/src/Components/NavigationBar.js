9ca5a80e733b025f1b6f2a48537af5b9073b6329
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-04-16 16:00:25 -0500

    Deleted build folder

--- client/src/Components/NavigationBar.js
+++ client/src/Components/NavigationBar.js
@@ -1,116 +1,185 @@
-import React from 'react'
+import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Box from "@material-ui/core/Box";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import DashboardRoundedIcon from '@material-ui/icons/DashboardRounded';
-import CalendarTodayOutlinedIcon from '@material-ui/icons/CalendarTodayOutlined';
-import AddIcon from '@material-ui/icons/Add';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import IconButton from '@material-ui/core/IconButton';
+import DashboardRoundedIcon from "@material-ui/icons/DashboardRounded";
+import CalendarTodayOutlinedIcon from "@material-ui/icons/CalendarTodayOutlined";
+import AddIcon from "@material-ui/icons/Add";
+import IconButton from "@material-ui/core/IconButton";
 import { NavLink } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { DropzoneDialog } from "material-ui-dropzone";
 
-const useStyles = makeStyles(theme => ({
-    appBar: {
-      background: "#FFFFFF",
-      boxShadow: "none",
-      flexDirection: "row",
-      alignItems: 'center',
-      height: '85px',
-    },
-    toolBar: {
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      flexGrow: 2,
-    },
-    buttonNotSelected: {
-      margin: theme.spacing(1),
-      color: '#666666',
-    },
-    selectedButton:{
-      margin: theme.spacing(1),
-      color: '#759DFD',
-    },
-    title: {
-      color: 'black',
-    }, 
-    buttonText: {
-      fontSize: 14,
-      fontWeight: 'bold',
-    },
-    buttonIcons: {
-      marginRight:'8px'
-    },
-    createButton: {
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    background: "#FFFFFF",
+    boxShadow: "none",
+    flexDirection: "row",
+    alignItems: "center",
+    height: "85px",
+  },
+  toolBar: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexGrow: 2,
+  },
+  buttonNotSelected: {
+    margin: theme.spacing(1),
+    color: "#666666",
+  },
+  selectedButton: {
+    margin: theme.spacing(1),
+    color: "#759DFD",
+  },
+  title: {
+    color: "black",
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  buttonIcons: {
+    marginRight: "8px",
+  },
+  createButton: {
+    backgroundColor: "#759DFD",
+    marginRight: "40px",
+    width: "175px",
+    "&:hover": {
       backgroundColor: "#759DFD",
-      marginRight: '40px',
-      width: '175px',
-      '&:hover': {
-        backgroundColor: '#759DFD'
-      }
     },
-    createButtonText: {
-      color: '#FFFFFF',
-      fontWeight: 'bold',
-      padding: '10px',
-    },
-    createButtonIcon: {
-      color: '#FFFFFF'
-    },
-    userIcon: {
-      width: '68px',
-      height: '68px',
-      flexShrink: 3,
-    }
-  }));
+  },
+  createButtonText: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    padding: "10px",
+  },
+  createButtonIcon: {
+    color: "#FFFFFF",
+  },
+  userIcon: {
+    width: "68px",
+    height: "68px",
+    flexShrink: 3,
+  },
+}));
 
 const NavigationBar = () => {
-    const classes = useStyles();
-    return(
-      <AppBar position="static" className={classes.appBar}>
-        <Toolbar className = {classes.toolBar}>
-          <Box >
-            <Button>
-            <Typography variant="title">
-              <img src="/images/logo.png" alt="bug"  />
-            </Typography>
-            </Button>  
-          </Box>
-          <Box >
-            <Button className = {classes.buttonNotSelected}
-              component = {NavLink}
-              to="/" activeClassName = {classes.selectedButton}>
-              <DashboardRoundedIcon className = {classes.buttonIcons}/> 
-              <Typography className = {classes.buttonText}>
-                Dashboard 
-              </Typography>
-            </Button>
-                <Button className = {classes.buttonNotSelected}
-                component = {NavLink}
-                to="/calendar" activeClassName = {classes.selectedButton}>
-                  <CalendarTodayOutlinedIcon className = {classes.buttonIcons}/> 
-                    <Typography className = {classes.buttonText}>
-                    Calendar 
-                    </Typography>
-                </Button>    
-          </Box>
-          <Box className >
-            <Button variant="contained" className = {classes.createButton}>
-              <AddIcon className = {classes.createButtonIcon}/> 
-              <Typography className = {classes.createButtonText}>
-              Create board
-              </Typography>
-            </Button>
-            <IconButton>
-              <AccountCircleIcon className = {classes.userIcon}/>
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    )
-}
+  const initialStateDropFile = {
+    open: false,
+    files: [],
+  };
 
-export default NavigationBar
+  const [anchorEl, open] = React.useState(null);
+
+  const handleClickAvatarMenu = (event) => {
+    open(event.currentTarget);
+  };
+  const handleCloseAvatarMenu = () => {
+    open(null);
+  };
+
+  const [stateDropFile, setStateDropFile] = React.useState(
+    initialStateDropFile
+  );
+
+  const handleOpenDropFile = () => {
+    setStateDropFile({
+      ...stateDropFile,
+      open: true,
+    });
+  };
+
+  const handleCloseDropFile = () => {
+    setStateDropFile({
+      ...stateDropFile,
+      open: false,
+    });
+
+    handleCloseAvatarMenu();
+  };
+
+  const handleSaveDropFile = (files) => {
+    setStateDropFile({
+      ...stateDropFile,
+      files: files,
+      open: false,
+    });
+  };
+
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static" className={classes.appBar}>
+      <Toolbar className={classes.toolBar}>
+        <Box>
+          <Button>
+            <Typography variant="title">
+              <img src="/images/logo.png" alt="bug" />
+            </Typography>
+          </Button>
+        </Box>
+        <Box>
+          <Button
+            className={classes.buttonNotSelected}
+            component={NavLink}
+            to="/"
+            activeClassName={classes.selectedButton}
+          >
+            <DashboardRoundedIcon className={classes.buttonIcons} />
+            <Typography className={classes.buttonText}>Dashboard</Typography>
+          </Button>
+          <Button
+            className={classes.buttonNotSelected}
+            component={NavLink}
+            to="/calendar"
+            activeClassName={classes.selectedButton}
+          >
+            <CalendarTodayOutlinedIcon className={classes.buttonIcons} />
+            <Typography className={classes.buttonText}>Calendar</Typography>
+          </Button>
+        </Box>
+        <Box className>
+          <Button variant="contained" className={classes.createButton}>
+            <AddIcon className={classes.createButtonIcon} />
+            <Typography className={classes.createButtonText}>
+              Create board
+            </Typography>
+          </Button>
+          <IconButton
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClickAvatarMenu}
+          >
+            <Avatar alt="Wonderful Client" src="/images/avatar.jpg" />
+          </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleCloseAvatarMenu}
+          >
+            <MenuItem onClick={handleOpenDropFile}>Change Photo</MenuItem>
+          </Menu>
+          <DropzoneDialog
+            open={stateDropFile.open}
+            onSave={handleSaveDropFile}
+            acceptedFiles={["image/jpeg", "image/png", "image/bmp"]}
+            showPreviews={true}
+            maxFileSize={5000000}
+            onClose={handleCloseDropFile}
+          />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavigationBar;
