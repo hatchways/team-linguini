9b831034be7f32fcacce8f7d90b0877bd9e3b3c7
aslamm <aslamm.anas29@gmail.com>
2020-04-17 00:29:54 -0400

    adding req functionality

---
+++ server/utils/sendEmail.js
@@ -0,0 +1,21 @@
+const http = require("https")
+
+const options = {
+    "method": "POST",
+    "hostname": "api.sendgrid.com",
+    "port": null,
+    "path": "/v3/mail/send",
+    "headers": {
+      "authorization": "Bearer process.env.SENDGRID_API_KEY",
+      "content-type": "application/json"
+    }
+}
+
+const req = http.request(options, (res) => {
+    const chunks = []
+
+    res.on("data", (chunk) => {
+        chunks.push(chunk)
+    })
+
+})
