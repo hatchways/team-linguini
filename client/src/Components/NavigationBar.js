99d107db506dc26e339d0bf75700adac298085a5
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-04-17 09:31:16 -0500

    Implemented some code clean up.

--- client/src/Components/NavigationBar.js
+++ client/src/Components/NavigationBar.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Button,
+  Typography,
+  IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
 import DashboardRoundedIcon from "@material-ui/icons/DashboardRounded";
 import CalendarTodayOutlinedIcon from "@material-ui/icons/CalendarTodayOutlined";
 import AddIcon from "@material-ui/icons/Add";
-import IconButton from "@material-ui/core/IconButton";
 import { NavLink } from "react-router-dom";
-import Avatar from "@material-ui/core/Avatar";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 import { DropzoneDialog } from "material-ui-dropzone";
 
 const useStyles = makeStyles((theme) => ({
@@ -76,18 +78,16 @@ const NavigationBar = () => {
     files: [],
   };
 
-  const [anchorEl, open] = React.useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const handleClickAvatarMenu = (event) => {
-    open(event.currentTarget);
+    setAnchorEl(event.currentTarget);
   };
   const handleCloseAvatarMenu = () => {
-    open(null);
+    setAnchorEl(null);
   };
 
-  const [stateDropFile, setStateDropFile] = React.useState(
-    initialStateDropFile
-  );
+  const [stateDropFile, setStateDropFile] = useState(initialStateDropFile);
 
   const handleOpenDropFile = () => {
     setStateDropFile({
