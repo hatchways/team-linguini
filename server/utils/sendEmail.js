96cd5cd7cb1b542c6abf4cf79a29df69e2c8dcb6
aslamm <aslamm.anas29@gmail.com>
2020-04-18 17:41:20 -0400

    aligned the code

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,21 +1,22 @@
-const sgMail = require("@sendgrid/mail")
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const send = async (message) => {
-    try{
-        await sgMail.send(message)
-    }catch{
-        error => console.log(error)
-    }
-}
+  try {
+    await sgMail.send(message);
+  } catch {
+    (error) => console.log(error);
+  }
+};
 
 const sendWelcomeEmail = async (to) => {
-        await send({
-            to,
-            from: 'aslamm.anas29@gmail.com',
-            subject: 'Welcome to Kanban Boards',
-            text: 'Thank you for registering on the kanban boards application. We welcome you to our community and look forward to seeing you around.',
-        })
-}
+  await send({
+    to,
+    from: "aslamm.anas29@gmail.com",
+    subject: "Welcome to Kanban Boards",
+    text:
+      "Thank you for registering on the kanban boards application. We welcome you to our community and look forward to seeing you around.",
+  });
+};
 
-module.exports = sendWelcomeEmail
+module.exports = sendWelcomeEmail;
