8ecf9abee8e5911d1d90494a7b9496c78100ee5c
aslamm <aslamm.anas29@gmail.com>
2020-04-21 01:42:00 -0400

    Custom styling the calendar

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -3,16 +3,22 @@ import { makeStyles } from "@material-ui/core/styles";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import { Container, Box, Grid } from "@material-ui/core";
+import "./Calendar.css"
 
 import BoardBar from "../components/BoardBar";
 import NavigationBar from "../components/NavigationBar";
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    height: "550px",
+    height: "600px",
     marginTop: "20px",
-    marginBottom: "20px",
+    marginLeft: "70px",
+    marginRight: "70px",
+    paddingBottom: "80px"
   },
+  calendar: {
+      background: "blue"
+  }
 }));
 
 const Calendar = () => {
@@ -21,7 +27,7 @@ const Calendar = () => {
     <div>
       <NavigationBar />
       <BoardBar />
-      <Container className={classes.container}>
+      <Box className={classes.container}>
         <FullCalendar
           defaultView="dayGridMonth"
           plugins={[dayGridPlugin]}
@@ -31,8 +37,21 @@ const Calendar = () => {
             center: "title",
             right: "prevYear, prev, next, nextYear",
           }}
+          events={[
+            { title: 'event 1', date: '2020-04-24' },
+            { title: 'event 2', date: '2020-04-27' },
+            { title: 'event 3', date: '2020-04-27' },
+            { title: 'event 4', date: '2020-04-27' },
+            { title: 'event 5', date: '2020-04-27' },
+            { title: 'event 6', date: '2020-04-27' },
+          ]}
+          eventLimit={true}
+          eventBackgroundColor='white'
+          eventTextColor='black'
+          eventBorderColor='lightgrey'
+          fixedWeekCount={false}
         />
-      </Container>
+      </Box>
     </div>
   );
 };
