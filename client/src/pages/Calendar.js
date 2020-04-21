9e5a05e976828b470409473fd7557e5fb51a9f0b
aslamm <aslamm.anas29@gmail.com>
2020-04-20 22:43:32 -0400

    Created the calendar

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,15 +1,38 @@
-import React from 'react'
-import BoardBar from '../components/BoardBar'
-import NavigationBar from '../components/NavigationBar'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { Container, Box, Grid } from "@material-ui/core";
+
+import BoardBar from "../components/BoardBar";
+import NavigationBar from "../components/NavigationBar";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: "550px",
+    marginTop: "20px",
+    marginBottom: "20px",
+  },
+}));
 
 const Calendar = () => {
-return(
+  const classes = useStyles();
+  return (
     <div>
-        <NavigationBar />
-        <BoardBar />
-        Calendar
+      <NavigationBar />
+      <BoardBar />
+      <Container className={classes.container}>
+        <FullCalendar
+          defaultView="dayGridMonth"
+          plugins={[dayGridPlugin]}
+          contentHeight={500}
+          header={{left:   '',
+          center: 'title',
+          right:  'today prev,next'}}
+        />
+      </Container>
     </div>
-)
-}
+  );
+};
 
-export default Calendar
+export default Calendar;
