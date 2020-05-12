ed4b3c5b38370de3e5c9cfc4322eaf2e6c41f5af
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-05-12 10:22:56 -0500

    Set up the log in page to match the registration page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -9,10 +9,9 @@ import {
 } from "../context/auth/auth.action";
 
 import { AuthForm, RedirectDiv } from "../components/auth";
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid, makeStyles, Typography, Box, Button } from "@material-ui/core";
 
 const Login = () => {
-
   const auth = useAuth();
   const { dispatchIsAuthenticated, dispatchUser } = auth;
   const [serverResponse, setServerResponse] = useState("");
@@ -64,7 +63,52 @@ const Login = () => {
         setServerResponse(e.message);
       });
   };
+  const onDemoSubmit = () => {
+    const { email, password } = {
+      email: "test@test.com",
+      password: "testtest",
+    };
 
+    setServerResponse("");
+
+    //Make request to check whether the email and password are valid
+    const url = "/api/v1/auth/login"; //It is mock data, it will change when sever can provide auth api
+
+    dispatchUser(fetchUserRequest());
+
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    };
+
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((res) => {
+        //If success to create a new account, redirect to login page
+        if (!res.error) {
+          //Save data on local storage
+          localStorage.setItem("isAuthenticated", true);
+          localStorage.setItem("user", JSON.stringify(res.user));
+          localStorage.setItem("token", JSON.stringify(res.token));
+
+          //Update the state of Auth providers
+          dispatchIsAuthenticated(setIsAuthenticated(true));
+          dispatchUser(fetchUserSuccess(res.user));
+          console.log("login successfully");
+
+          //Redirect to dashboard
+          window.location.replace("/");
+        } else {
+          throw Error(res.error);
+        }
+      })
+      .catch((e) => {
+        // console.log(e);
+        dispatchUser(fetchUserFailure(e.message));
+        setServerResponse(e.message);
+      });
+  };
   return (
     <Grid container className={classes.vh100}>
       <Grid item md={6} xs={12} className={classes.img}>
@@ -84,6 +128,22 @@ const Login = () => {
           link={"/signup"}
           desc={"Create"}
         />
+        <Box className={classes.demoContainer} mt={3} mb={2}>
+          <Typography className={classes.demoDescription}>
+            Do not want to create an account. Click Below:
+          </Typography>
+        </Box>
+        <Box className={classes.demoContainer}>
+          <Button
+            variant="contained"
+            onClick={onDemoSubmit}
+            className={classes.demoButton}
+          >
+            <Typography className={classes.demoDescription}>
+              Try Demo!
+            </Typography>
+          </Button>
+        </Box>
       </Grid>
     </Grid>
   );
